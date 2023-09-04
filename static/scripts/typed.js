f419b829d8f1791d20c16c250641eeea759dda79
Can Kolho <can.kolho@hotmail.com>
2023-09-04 09:39:42 +0300

    Landing page theme changes

--- static/scripts/typed.js
+++ static/scripts/typed.js
@@ -1,5 +1,6 @@
 const typed = new Typed('.multiple-text', {
-  strings: ['Laundry room.', 'Drying room.',],
+  /*strings: ['Laundry room.', 'Drying room.',],*/
+  strings: ['easier!', 'faster!', 'smarter!'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
