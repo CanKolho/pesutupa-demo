015e70c2531e1e8d8ad9e643fbdb82490c7be56c
Can Kolho <can.kolho@hotmail.com>
2023-10-13 17:42:23 +0300

    config file modified to correct form

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 export const DATABASE_URL = Deno.env.get("DATABASE_URL");
-export const SEND_EMAIL = Deno.env.get("SEND_EMAIL");
-export const PASSWORD = Deno.env.get("PASSWROD");
+export const SEND_EMAIL = `${Deno.env.get("SEND_EMAIL")}`;
+export const PASSWORD = `$${Deno.env.get("PASSWROD")}`;
 
 const key = await crypto.subtle.generateKey(
   { name: "HMAC", hash: "SHA-512" },
